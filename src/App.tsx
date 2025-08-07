f1126cdbd9fbf441982b1470ea4335407a28e12c
georgeygigz <georgeymutti@gmail.com>
2025-08-07 23:57:26 +0300

    Ensure all action are visible

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { AuthProvider } from './context/AuthContext';
 import Dashboard from './pages/Dashboard';
+import Auth from './pages/Auth';
 
 function App() {
   return (
     <AuthProvider>
       <Dashboard />
+      {/* <Auth /> */}
     </AuthProvider>
   );
 }
